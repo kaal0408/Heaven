d2a39e27c1263564c6a3913a3edc29cae53e9bf4
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-05-20 17:25:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from pyrogram import idle
+from pyrogram import Client as Bot
+
+from callsmusic import run
+from config import API_ID, API_HASH, OWNER_IDS
+
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
+
+bot.start()
+run()
+idle()
